46e5de03d3ee76a3ebae8838fbb6329b1682a658
barimale <mateusz.wolnica@gmail.com>
2021-05-07 00:20:51 +0200

    Initial commit.

---
+++ Albergue.Administrator/Startup.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Albergue.Administrator
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            //app.UseHsts();
+
+            app.UseCors(p =>
+                p
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello Hello Hello bye bye bye!");
+                });
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,7 +1,17 @@
-# DEV stage
-local instance of Docker
-# UAT stage
-deployment to Heroku
-# PROD stage
-manual deployment to already existed server
-- install MS SQL Server 2019 Express
+# Mini-server configuration
+```
+Public IP: 94.132.173.156
+Local IP: 192.168.2.100
+
+username: albergue
+password: albergue 
+```
+
+```
+scp -r ./bin/Release/net5.0/publish/* albergue@192.168.2.100:/var/www/albergue.administrator
+
+sudo cp ./Properties/albergue-administrator.service /etc/systemd/system/albergue-administrator.service
+sudo systemctl daemon-reload
+```
+
+https://localhost:5021;
